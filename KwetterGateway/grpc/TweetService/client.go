17ad38d87f99b7e4da790ad5c5b8ed625ca12b76
Evers419 <78910809+Evers419@users.noreply.github.com>
2023-04-16 23:40:17 +0200

    Update client.go

--- KwetterGateway/grpc/TweetService/client.go
+++ KwetterGateway/grpc/TweetService/client.go
@@ -1,18 +1,19 @@
 package TweetService
 
 import (
+	pbtweet "github.com/Portfolio-Adv-Software/Kwetter/KwetterGateway/grpc/TweetService/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
-	"github.com/Portfolio-Adv-Software/Kwetter/KwetterGateway/grpc/TweetService/proto"
 )
 
 func InitClient() {
 	// Set up a gRPC client connection to your backend service
-	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(":50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to dial: %v", err)
+		log.Fatalf("could not connect: %s", err)
 	}
 	defer conn.Close()
-	client := pbtweet.
+	client := pbtweet.NewTweetServiceClient(conn)
+
 }
