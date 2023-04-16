117f1fa307c6424eb4c20a51a436c2cbf21627a3
Evers419 <78910809+Evers419@users.noreply.github.com>
2023-04-16 23:43:48 +0200

    Update client.go

--- KwetterGateway/grpc/TweetService/client.go
+++ KwetterGateway/grpc/TweetService/client.go
@@ -1,6 +1,8 @@
 package TweetService
 
 import (
+	"context"
+	"fmt"
 	pbtweet "github.com/Portfolio-Adv-Software/Kwetter/KwetterGateway/grpc/TweetService/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -14,6 +16,15 @@ func InitClient() {
 		log.Fatalf("could not connect: %s", err)
 	}
 	defer conn.Close()
-	client := pbtweet.NewTweetServiceClient(conn)
+	c := pbtweet.NewTweetServiceClient(conn)
 
+}
+
+func ReturnAll(c pbtweet.TweetServiceClient) (response pbtweet.TweetService_ReturnAllClient) {
+	response, err := c.ReturnAll(context.Background(), &pbtweet.ReturnAllReq{})
+	if err != nil {
+		log.Fatalf("Failed to call ReturnAll: %v", err)
+	}
+	fmt.Print(response)
+	return response
 }
